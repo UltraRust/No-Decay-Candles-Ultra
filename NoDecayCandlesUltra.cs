a3432c1cc470f05fb664013ef7765c54f55aef9d
UltraRust <53350204+UltraRust@users.noreply.github.com>
2019-08-08 20:44:21 +0200

    Add files via upload

---
+++ NoDecayCandlesUltra.cs
@@ -0,0 +1,14 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("NoDecayCandlesUltra", "Ultra", "1.1.2")]
+    [Description("Candles don't get damage by decay")]
+
+    class NoDecayCandlesUltra : RustPlugin
+    {
+        object OnEntityTakeDamage(BaseCombatEntity entity, HitInfo hitInfo)
+        {
+            if (entity.ShortPrefabName.Contains("candles")) return 0;            
+            return null;
+        }
+    }
+}
